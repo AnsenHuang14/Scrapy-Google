b1397e43a4b723bba6b3ec2928208651b1483164
AnsenHuang14 <ansen274@gmail.com>
2017-07-05 18:02:53 +0800

    update google

--- AndroidAppScrapy/AndroidAppScrapy/items.py
+++ AndroidAppScrapy/AndroidAppScrapy/items.py
@@ -1,18 +1,52 @@
-# -*- coding: utf-8 -*-
+# -*- coding :  utf-8 -*-
 
 # Define here the models for your scraped items
 #
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+# See documentation in = 
+# http ://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
-class AndroidappscrapyItem(scrapy.Item):
-    # define the fields for your item here like:
+class AndroidappscrapyItem(scrapy.Item) :
+    # define the fields for your item here like: 
     # name = scrapy.Field()
-    title = scrapy.Field()    # 标题
-    link = scrapy.Field()     # 链接
-    desc = scrapy.Field()     # 简述
-    posttime = scrapy.Field() # 发布时间
-    pass
+	link = scrapy.Field()
+	category = scrapy.Field()
+	
+	PkgName = scrapy.Field()
+	Title = scrapy.Field()
+	genre = scrapy.Field()
+	score = scrapy.Field()	
+	ratingHistogram = scrapy.Field()
+	comp = scrapy.Field()
+	datePublished = scrapy.Field()
+	numDownloads = scrapy.Field()
+	ratingValue = scrapy.Field()
+	contentRating = scrapy.Field()
+	Reviewers = scrapy.Field()
+	softwareVersion = scrapy.Field()
+	GpCategory = scrapy.Field()
+	GpCategory2 = scrapy.Field()
+	similarApps = scrapy.Field()
+	operatingSystems = scrapy.Field()
+	ads = scrapy.Field()
+	whatsNew = scrapy.Field()
+	inAppMsg = scrapy.Field()
+	price = scrapy.Field()
+	LogoUrl = scrapy.Field()
+	ClkUrl = scrapy.Field()
+	mail = scrapy.Field()
+	Thumbnails = scrapy.Field() # screenshot
+	Desc = scrapy.Field() # description
+	Rating = scrapy.Field() #width
+	Recommenders = scrapy.Field() #empty in java
+	PosId = scrapy.Field() #empty in java
+	AppType = scrapy.Field()
+	Lan = scrapy.Field()
+	Country = scrapy.Field() # set by crawler
+	fileSize =  scrapy.Field()# cant find tag due to new version
+	genre2 =  scrapy.Field()# cant find tag due to new version
+	name = scrapy.Field() # same as title 
+	GpTag = scrapy.Field()# cant find tag due to new version
+	pass
--- AndroidAppScrapy/AndroidAppScrapy/spiders/App_Spider.py
+++ AndroidAppScrapy/AndroidAppScrapy/spiders/App_Spider.py
@@ -1,17 +1,80 @@
 from AndroidAppScrapy.items import AndroidappscrapyItem
 import scrapy
 
+
+def read_url(path='.\\URL.txt'):
+    start_urls = list()
+    with open(path, 'rb') as f:
+        start_urls.append(f.read())
+    start_urls = start_urls[0].split('\n')
+    return start_urls
+
 class App_Spider(scrapy.Spider):
     name = "App_Spider"
-    allowed_domains = ["huxiu.com"]
-    start_urls = [
-        "http://www.huxiu.com/index.php"
-    ]
+    start_urls = read_url()
+    allowed_domains = ["play.google.com"]
+    
     def parse(self, response):
-        for sel in response.xpath('//div[@class="mod-info-flow"]/div/div[@class="mob-ctt"]'):
-            item = AndroidappscrapyItem()
-            item['title'] = sel.xpath('h3/a/text()')[0].extract()
-            item['link'] = sel.xpath('h3/a/@href')[0].extract()
-            url = response.urljoin(item['link'])
-            item['desc'] = sel.xpath('div[@class="mob-sub"]/text()')[0].extract()
-            print(item['title'],item['link'],item['desc'])
+        app = AndroidappscrapyItem()
+        app['PkgName'] = response.url.split('=')[1].replace('&hl','')
+        app['Title'] = response.xpath('//div[@class="id-app-title"]/text()')[0].extract()
+        app['genre'] = response.xpath('//span[@itemprop="genre"]/text()')[0].extract()
+        app['score'] = response.xpath('//div[@class="score"]/text()')[0].extract()
+        
+        score_number = list()
+        for s in response.xpath('//span[@class="bar-number"]/text()'):
+            score_number.append(s.extract().replace(',',''))
+        
+        app['ratingHistogram'] = score_number
+        app['comp'] = response.xpath('//span[@itemprop="name"]/text()')[0].extract()
+        app['datePublished'] = response.xpath('//div[@itemprop="datePublished"]/text()')[0].extract()
+        app['numDownloads'] = response.xpath('//div[@itemprop="numDownloads"]/text()')[0].extract()
+        app['ratingValue'] =  response.xpath('//div[@itemprop="contentRating"]/text()')[0].extract()
+        app['contentRating'] = app['ratingValue'] 
+        app['Reviewers'] = response.xpath('//span[@class="reviews-num"]/text()')[0].extract()
+        app['softwareVersion'] = response.xpath('//div[@itemprop="softwareVersion"]/text()')[0].extract()
+        app['GpCategory'] =  app['genre']
+        app['GpCategory2'] = app['genre']
+
+        similar_app = list()
+        for s in response.xpath('//span[@class="preview-overlay-container"]//@data-docid'):
+            similar_app.append(s.extract())
+        
+        app['similarApps'] = similar_app
+        app['operatingSystems'] = response.xpath('//div[@itemprop="operatingSystems"]/text()')[0].extract().replace(' ','')
+        app['ads'] = response.xpath('//span[@class="ads-supported-label-msg"]/text()')[0].extract()
+        app['whatsNew'] = response.xpath('//div[@class="recent-change"]/text()')[0].extract()
+        if len(response.xpath('//div[@class="inapp-msg"]/text()'))==0:
+            app['inAppMsg'] = ''
+        else:
+            app['inAppMsg'] = response.xpath('//div[@class="inapp-msg"]/text()')[0].extract()
+        app['price'] =  response.xpath('//meta[@itemprop="price"]//@content')[0].extract()
+        app['LogoUrl'] =  response.xpath('//img[@class="cover-image"]//@src')[0].extract()
+        app['ClkUrl'] =  response.url
+        app['mail'] =  response.xpath('//a[@class="dev-link"]//@href')[1].extract().split(':')[1]
+        app['Thumbnails'] =  response.xpath('//img[@itemprop="screenshot"]//@src')[0].extract()
+        app['Desc'] =  response.xpath('//div[@jsname="C4s9Ed"]/text()')[0].extract()
+        app['Rating'] =  response.xpath('//div[@class="current-rating"]//@style')[0].extract().replace(';','').replace('width','').replace(' ','')
+        app['Recommenders'] = '' 
+        app['PosId'] =  ''
+        app['AppType'] =  app['Title']+'/'+ app['genre']
+        app['Lan'] =  response.url.split('=')[2]
+        app['Country'] =  response.url.split('=')[2]
+        app['fileSize'] =  ''
+        app['genre2'] =  app['genre']
+        app['name'] =  app['Title']
+        app['GpTag'] =  ''
+        # print (app['PkgName'],app['Title'],app['genre'],app['score'],
+        #     app['ratingHistogram'],app['comp'],app['datePublished'], 
+        #     app['numDownloads'],app['ratingValue'],app['contentRating'],
+        #     app['Reviewers'],app['softwareVersion'],app['GpCategory'],
+        #     app['GpCategory2'] , app['similarApps'], app['operatingSystems'],
+        #     app['ads'],app['whatsNew'], app['inAppMsg'],
+        #     app['price'],app['LogoUrl'],app['ClkUrl'],app['mail'],app['Thumbnails'],
+        #     app['Desc'],app['Rating'],app['Recommenders'],app['PosId'],app['AppType'],
+        #     app['Lan'], app['Country'],app['fileSize'],app['genre2'],app['name'],app['GpTag'])
+        yield app
+        
+
+
+
